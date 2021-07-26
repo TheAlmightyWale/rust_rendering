d86c302a9e9d25f2ae1b17fb9a45df243be3f191
Jarryd Peterson <Thealmightywale@gmail.com>
2021-07-25 19:33:25 -0700

    Rendering texture covering entire screen, using a single triangle

--- src/state.rs
+++ src/state.rs
@@ -153,30 +153,23 @@ impl State {
             },
         });
 
+        //One triangle that will automatically get clipped to screen size, with a texture stretched to the internal "square" that is actually displayed
         const VERTICES: &[Vertex] = &[
             Vertex {
-                position: [-0.0868241, 0.49240386, 0.0],
-                tex_coords: [0.4131759, 0.99240386],
-            }, // A
+                tex_coords: [0.0, 0.0],
+                position: [-1.0, -1.0, 0.0],
+            },
             Vertex {
-                position: [-0.49513406, 0.06958647, 0.0],
-                tex_coords: [0.0048659444, 0.56958646],
-            }, // B
+                tex_coords: [2.0, 0.0],
+                position: [3.0, -1.0, 0.0],
+            },
             Vertex {
-                position: [-0.21918549, -0.44939706, 0.0],
-                tex_coords: [0.28081453, 0.050602943],
-            }, // C
-            Vertex {
-                position: [0.35966998, -0.3473291, 0.0],
-                tex_coords: [0.85967, 0.15267089],
-            }, // D
-            Vertex {
-                position: [0.44147372, 0.2347359, 0.0],
-                tex_coords: [0.9414737, 0.7347359],
-            }, // E
+                tex_coords: [0.0, 2.0],
+                position: [-1.0, 3.0, 0.0],
+            },
         ];
 
-        const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4, /* padding */ 0]; //WGPU needs buffers to be aligned to 4 bytes
+        const INDICES: &[u16] = &[0, 1, 2, 0]; //WGPU needs buffers to be aligned to 4 bytes
 
         let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("Vertex Buffer"),
